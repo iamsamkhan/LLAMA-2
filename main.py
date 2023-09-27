ba3bbe9b6046979c7d055a6e1e7bfc6d03bbb254
unknown <smshad0001@gamil.com>
2023-09-28 00:52:17 +0530

    add file

---
+++ main.py
@@ -0,0 +1,125 @@
+from torch import cuda, bfloat16
+from transformers import require_version, require_version_core
+
+import transformers
+
+model_id = 'meta-llama/Llama-2-70b-chat-hf'
+
+device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+
+# set quantization configuration to load large model with less GPU memory
+# this requires the `bitsandbytes` library
+bnb_config = transformers.BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=bfloat16
+)
+
+# begin initializing HF items, need auth token for these
+hf_auth = ''
+model_config = transformers.AutoConfig.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth
+)
+
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    config=model_config,
+    quantization_config=bnb_config,
+    device_map='auto',
+    use_auth_token=hf_auth
+)
+model.eval()
+print(f"Model loaded on {device}")
+
+
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth
+)
+    
+generate_text = transformers.pipeline(
+    model=model, tokenizer=tokenizer,
+    return_full_text=True,  # langchain expects the full text
+    task='text-generation',
+    # we pass model parameters here too
+    temperature=0.0,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+    max_new_tokens=512,  # mex number of tokens to generate in the output
+    repetition_penalty=1.1  # without this output begins repeating
+)
+
+
+res = generate_text("Explain to me the difference between nuclear fission and fusion.")
+print(res[0]["generated_text"])
+     
+
+
+from langchain.llms import HuggingFacePipeline
+
+llm = HuggingFacePipeline(pipeline=generate_text)
+llm(prompt="Explain to me the difference between nuclear fission and fusion.")
+
+#Initializing an Conversational Agent
+
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.agents import load_tools
+
+memory = ConversationBufferWindowMemory(
+    memory_key="chat_history", k=5, return_messages=True, output_key="output"
+)
+tools = load_tools(["llm-math"], llm=llm)
+
+
+
+from langchain.agents import AgentOutputParser
+from langchain.agents.conversational_chat.prompt import FORMAT_INSTRUCTIONS
+from langchain.output_parsers.json import parse_json_markdown
+from langchain.schema import AgentAction, AgentFinish
+
+class OutputParser(AgentOutputParser):
+    def get_format_instructions(self) -> str:
+        return FORMAT_INSTRUCTIONS
+
+    def parse(self, text: str) -> AgentAction | AgentFinish:
+        try:
+            # this will work IF the text is a valid JSON with action and action_input
+            response = parse_json_markdown(text)
+            action, action_input = response["action"], response["action_input"]
+            if action == "Final Answer":
+                # this means the agent is finished so we call AgentFinish
+                return AgentFinish({"output": action_input}, text)
+            else:
+                # otherwise the agent wants to use an action, so we call AgentAction
+                return AgentAction(action, action_input, text)
+        except Exception:
+            # sometimes the agent will return a string that is not a valid JSON
+            # often this happens when the agent is finished
+            # so we just return the text as the output
+            return AgentFinish({"output": text}, text)
+
+    @property
+    def _type(self) -> str:
+        return "conversational_chat"
+
+# initialize output parser for agent
+parser = OutputParser()
+
+from langchain.agents import initialize_agent
+
+# initialize agent
+agent = initialize_agent(
+    agent="chat-conversational-react-description",
+    tools=tools,
+    llm=llm,
+    verbose=True,
+    early_stopping_method="generate",
+    memory=memory,
+    #agent_kwargs={"output_parser": parser}
+)
+
+
+agent.agent.llm_chain.prompt
+
